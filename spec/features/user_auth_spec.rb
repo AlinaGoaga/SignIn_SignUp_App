3a7d6481e58df96f1f46f7bacef08dc43dcd87dd
Alina Goaga <goaga.alina@gmail.com>
2019-01-08 21:35:39 +0000

    Feature tests updated

--- spec/features/user_auth_spec.rb
+++ spec/features/user_auth_spec.rb
@@ -8,7 +8,7 @@ RSpec.feature 'User Authentication' do
       click_button 'Sign up'
 
       expect(page).to have_content 'Welcome, test@test.com'
-
+    end
     scenario "user can't sign up twice with the same email" do
       visit '/'
       click_on 'Sign up'
@@ -20,44 +20,43 @@ RSpec.feature 'User Authentication' do
       fill_in :email, with: 'test@test.com'
       fill_in :password, with: 'secret123'
       click_button 'Sign up'
-      expect(page).to raise_error 'Sorry, that email address is taken'
+      expect(page).to have_content 'Sorry, that email address is taken'
     end
   end
 
-    scenario 'A user cant signup twice with the same email' do
+  scenario 'A user cant signup twice with the same email' do
+    visit '/'
+    click_on 'Sign up'
+    fill_in :email, with: 'test@test.com'
+    fill_in :password, with: 'secret123'
+    click_button 'Sign up'
+
+    expect(page).to have_content 'Welcome, test@test.com'
+  end
+
+  context 'Sign in/out' do
+    let!(:user) { User.create(email: 'test@test.com', password: 'secret123') }
+
+    scenario 'A user can signin' do
       visit '/'
-      click_on 'Sign up'
+      click_on 'Sign in'
       fill_in :email, with: 'test@test.com'
       fill_in :password, with: 'secret123'
-      click_button 'Sign up'
+      click_button 'Sign in'
 
       expect(page).to have_content 'Welcome, test@test.com'
-
     end
 
-    context 'Sign in/out' do
-      let!(:user) { User.create(email: 'test@test.com', password: 'secret123')}
+    scenario 'A signed in user can log out' do
+      visit '/signin'
+      fill_in :email, with: 'test@test.com'
+      fill_in :password, with: 'secret123'
+      click_button 'Sign in'
+      click_button 'Log out'
 
-      scenario 'A user can signin' do
-        visit '/'
-        click_on 'Sign in'
-        fill_in :email, with: 'test@test.com'
-        fill_in :password, with: 'secret123'
-        click_button 'Sign in'
-
-        expect(page).to have_content 'Welcome, test@test.com'
-      end
-
-      scenario 'A signed in user can log out' do
-        visit '/signin'
-        fill_in :email, with: 'test@test.com'
-        fill_in :password, with: 'secret123'
-        click_button 'Sign in'
-        click_button 'Log out'
-
-        expect(page.current_path).to eq '/'
-        expect(page).to have_content 'Sign up'
-        expect(page).to have_content 'Sign in'
-      end
+      expect(page.current_path).to eq '/'
+      expect(page).to have_content 'Sign up'
+      expect(page).to have_content 'Sign in'
     end
   end
+end
